f299c6fe473bc39a5610be97e6f5ef163423ddb3
eksite <denisandipod@gmail.com>
2021-04-13 21:38:14 +0300

    WIP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import useLoadData from "../hooks/useLoadData.jsx";
 import { FixedSizeList as List } from "react-window";
-
-const Row = ({ index, style }) => <div style={style}>Row {index}</div>;
+import AutoSizer from "react-virtualized-auto-sizer";
+import "./styles.css";
 
 const App = () => {
   const data = useLoadData("data.json");
   console.log(data);
+  const Row = ({ index, style }) => (
+    <div style={style}>
+      {/*  */}
+      {data ? <>{console.log(data[index].age)}</> : smth}
+    </div>
+  );
   return data ? (
-    <List height={150} itemCount={data.length} itemSize={35} width={300}>
-      {Row}
-    </List>
+    <div style={{ height: "100%", width: "100%" }}>
+      <AutoSizer>
+        {({ height, width }) => (
+          <List
+            className="List"
+            height={height}
+            itemCount={1000}
+            itemSize={50}
+            width={width}
+          >
+            {Row}
+          </List>
+        )}
+      </AutoSizer>
+    </div>
   ) : (
     <div>loading...</div>
   );

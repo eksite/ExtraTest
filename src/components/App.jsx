8f51eb3f043818961e5983c3afcfe7e4fc4b6152
eksite <denisandipod@gmail.com>
2021-04-19 04:50:08 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,13 @@ import { loadFromJson } from "../redux/dataSlice.jsx";
 import Header from "./Header.jsx";
 import Page from "./Page.jsx";
 
+const URL = "https://raw.githubusercontent.com/eksite/ExtraTest/master/data.json"
+
 const App = () => {
   const dispatch = useDispatch();
 
   const loadData = async () => {
-    const result = await fetch("data.json").then((res) => res.json());
+    const result = await fetch(URL).then((res) => res.json());
     dispatch(loadFromJson({ data: result }));
   };
 
